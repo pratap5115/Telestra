e39f3ddb710e3bba81dd21fa231c15556e64ce2d
Divya <Divya@Divyas-iMAC.local>
2016-05-20 11:35:48 +0530

    Initial Commit

---
+++ Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment
+//
+//  Created by Divya on 20/05/16.
+//  Copyright © 2016 Divya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment
+//
+//  Created by Divya on 20/05/16.
+//  Copyright © 2016 Divya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
